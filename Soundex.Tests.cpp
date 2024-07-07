a2b504c7ff70d6580c50f0eea7b2fec25ed7296c
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-07 12:40:47 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,14 +1,14 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_1) {
  //AAA
   char soundex[5];
   generateSoundex("AX", soundex);
  ASSERT_STREQ(soundex,"A200");
 }
 
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_2) {
  //AAA
   char soundex[5];
   generateSoundex("A", soundex);
